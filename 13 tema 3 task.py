94c6fee06fb387ccbb46b489b027348a09f26a98
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-02-03 20:34:21 +0200

    13 tema 3 task

---
+++ 13 tema 3 task.py
@@ -0,0 +1,17 @@
+import datetime
+
+print("---------Užduotis 5: Datos ir laiko įvedimas naudojant str formatą-----------")
+
+user_input = input("Įveskite datą (YYYY-MM-DD): ")
+input_date = datetime.datetime.strptime(user_input, "%Y-%m-%d")
+
+print("Jūsų įvesta data datetime formatu:", input_date)
+
+print("---------Užduotis 6: Datos ir laiko išvedimas naudojant strftime formatą------")
+
+datetime_obj = datetime.datetime(2022, 12, 31, 23, 59, 59)
+formatted_date_1 = datetime_obj.strftime("%d/%m/%Y %H:%M:%S")
+formatted_date_2 = datetime_obj.strftime("%Y year %B %d day")
+
+print(formatted_date_1)
+print(formatted_date_2)
