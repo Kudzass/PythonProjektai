c6cbb729466b474988251a9bc46246a4d372f88a
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-02-03 20:53:00 +0200

    13 tema 3 task pataisytas

--- 13 tema 3 task.py
+++ 13 tema 3 task.py
@@ -1,4 +1,6 @@
 import datetime
+import locale
+locale.setlocale(locale.LC_TIME, 'lt_LT.UTF-8')
 
 print("---------Užduotis 5: Datos ir laiko įvedimas naudojant str formatą-----------")
 
@@ -10,8 +12,11 @@ print("Jūsų įvesta data datetime formatu:", input_date)
 print("---------Užduotis 6: Datos ir laiko išvedimas naudojant strftime formatą------")
 
 datetime_obj = datetime.datetime(2022, 12, 31, 23, 59, 59)
+
 formatted_date_1 = datetime_obj.strftime("%d/%m/%Y %H:%M:%S")
-formatted_date_2 = datetime_obj.strftime("%Y year %B %d day")
+formatted_date_2 = datetime_obj.strftime("%Y metų %B %d diena")
+formatted_date_2 = formatted_date_2.replace("metu", "metų")
+formatted_date_2 = formatted_date_2.replace("gruodis", "gruodžio")
 
 print(formatted_date_1)
 print(formatted_date_2)
