68630d3e17993aca872ae7a1b7f59162b057e3ff
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-02-17 19:55:48 +0200

    21 tema 1 task

--- 21 tema 1 task.py
+++ 21 tema 1 task.py
@@ -1,1 +1,30 @@
 import sqlalchemy
+
+
+from sqlalchemy import Column, create_engine, Integer, String, Float
+from sqlalchemy.orm import declarative_base
+
+
+engine = create_engine('sqlite:///mokykla.db')
+Base = declarative_base()
+
+
+class Mokinys(Base):
+    __tablename__ = 'mokinys'
+
+    id = Column(Integer, primary_key=True)
+    vardas = Column(String)
+    pavarde = Column(String)
+    klase = Column(Integer)
+
+
+class Mokytojas(Base):
+    __tablename__ = 'mokytojas'
+
+    id = Column(Integer, primary_key=True)
+    vardas = Column(String)
+    pavarde = Column(String)
+    dalykas = Column(String)
+
+
+Base.metadata.create_all(engine)
